2614f1ffc73b824e92b02b9b604fd7d758d0d848
liuyi <121407741@qq.com>
2018-03-15 21:16:57 +0800

    add a console runner

---
+++ Demo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TaskSchedule;
+
+namespace Demo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TaskScheduleRegister.RegistTask(
+                "timer", 
+                () => Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")),
+                TimeSpan.FromSeconds(5));
+
+            Console.ReadLine();
+        }
+    }
+}
